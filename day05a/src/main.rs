171defb36af4669fd9473b4feeb44af12e0780d6
John Peterson <john.peterson@globality.com>
2022-12-06 09:01:29 +0000

    Finishing day05a

--- day05a/src/main.rs
+++ day05a/src/main.rs
@@ -1,39 +1,10 @@
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
 };
-
-struct Stack<T> {
-    stack: Vec<T>,
-}
-
-impl<T> Stack<T> {
-    fn new() -> Self {
-        Stack { stack: Vec::new() }
-    }
-
-    fn length(&self) -> usize {
-        self.stack.len()
-    }
-
-    fn pop(&mut self) -> Option<T> {
-        self.stack.pop()
-    }
-
-    fn push(&mut self, item: T) {
-        self.stack.push(item)
-    }
-
-    fn is_empty(&self) -> bool {
-        self.stack.is_empty()
-    }
-
-    fn peek(&self) -> Option<&T> {
-        self.stack.last()
-    }
-}
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file");
@@ -51,21 +22,14 @@ fn contains_move(line: &String) -> bool {
     return line.contains("move");
 }
 
-fn create_stacks(lines: &Vec<String>) -> HashMap<isize, Stack<String>> {
+fn create_stacks(lines: &Vec<String>) -> HashMap<isize, Vec<String>> {
     // e.g line = '[Z] [M] [P]'
-    // format = 3 chars then break, 3 cars then break
-
     // So we can create a map -> Vec
 
     // First we create the list map
     let mut stacks_list_map: HashMap<isize, Vec<String>> = HashMap::new();
     for line in lines {
-        // let mut next_val = 1;
         for (i, c) in line.chars().enumerate() {
-            // println!("index: {}", i as isize);
-            // println!("char: /{}/", c as char);
-            // println!("next_val: /{}/", next_val);
-
             if i % 4 == 1 && (c as char).to_string() != " " {
                 let map_index = (i - 1) / 4;
                 // We've found a value so we should add it to one of our stack lists
@@ -75,33 +39,21 @@ fn create_stacks(lines: &Vec<String>) -> HashMap<isize, Stack<String>> {
                         .get(&(map_index as isize))
                         .unwrap()
                         .to_owned();
-                    // existing_arr.push((c as char).to_string());
                 }
                 existing_arr.push((c as char).to_string());
-                // println!("index: {}", i as isize);
-                // println!("char: /{}/", c as char);
                 stacks_list_map.insert((map_index as isize), existing_arr.to_owned());
-                // next_val = next_val + 4;
             }
         }
     }
-    // println!("{}", stacks_list_map.len());
-    //
-    // for (key, value) in &stacks_list_map {
-    //     println!("{}", key);
-    //     for val in value.iter() {
-    //         println!("{}", val)
-    //     }
-    // }
 
-    // then we just reverse the vecs and push onto stacks?
-    let mut stacks_map: HashMap<isize, Stack<String>> = HashMap::new();
+    // then we just reverse the vecs and push onto stacks
+    let mut stacks_map: HashMap<isize, Vec<String>> = HashMap::new();
 
     for i in 0..stacks_list_map.len() {
         let mut existing_arr = stacks_list_map.get(&(i as isize)).unwrap().to_owned();
         existing_arr.reverse();
 
-        let mut stack: Stack<String> = Stack::new();
+        let mut stack: Vec<String> = Vec::new();
         for item in existing_arr {
             stack.push(item)
         }
@@ -112,36 +64,83 @@ fn create_stacks(lines: &Vec<String>) -> HashMap<isize, Stack<String>> {
     return stacks_map;
 }
 
-fn move_stacks(count: isize, start: isize, end: isize, stacks: HashMap<isize, Stack<String>>) {}
+fn move_stacks(
+    count: isize,
+    start: isize,
+    end: isize,
+    stacks: &mut HashMap<isize, Vec<String>>,
+) -> &mut HashMap<isize, Vec<String>> {
+    // We need the indexes
+    let i_start = start - 1;
+    let i_end = end - 1;
+
+    if start == end {
+        return stacks;
+    }
+
+    let mut starting_stack = stacks.get(&i_start).unwrap().to_owned();
+    let mut end_stack = stacks.get(&i_end).unwrap().to_owned();
+
+    for i in 0..count {
+        let item_to_move = starting_stack.pop().unwrap();
+        end_stack.push(item_to_move);
+    }
+
+    stacks.insert(i_start, starting_stack);
+    stacks.insert(i_end, end_stack);
+
+    return stacks;
+}
+
+fn get_move_defintion(line: &String) -> (isize, isize, isize) {
+    let split: Vec<String> = line.split(" ").map(|a| a.to_string()).collect();
+
+    let move_count: isize = Some(split.get(1))
+        .unwrap()
+        .expect("couldn't get value from line")
+        .parse()
+        .unwrap();
+    let start: isize = Some(split.get(3))
+        .unwrap()
+        .expect("couldn't get value from line")
+        .parse()
+        .unwrap();
+    let stop: isize = Some(split.get(5))
+        .unwrap()
+        .expect("couldn't get value from line")
+        .parse()
+        .unwrap();
+
+    return (move_count, start, stop);
+}
 
 fn main() {
     // This gets the initial blocks
-    let blocks: Vec<String> = lines_from_file("./example.txt")
+    let blocks: Vec<String> = lines_from_file("./input.txt")
         .iter()
         .filter(|line| contains_block(line))
         .map(|a| a.to_owned())
         .collect::<Vec<String>>();
 
-    let moves: Vec<String> = lines_from_file("./example.txt")
+    let moves: Vec<String> = lines_from_file("./input.txt")
         .iter()
         .filter(|line| contains_move(line))
         .map(|a| a.to_owned())
         .collect::<Vec<String>>();
 
-    let stacks = create_stacks(&blocks);
+    let mut stacks = create_stacks(&blocks);
 
-    for (key, value) in stacks {
-        println!("{}", key);
-        for val in value.stack.iter() {
-            println!("{}", val)
-        }
+    let stack_map = moves
+        .iter()
+        .map(|line| get_move_defintion(line))
+        .fold(&mut stacks, |stacks, (a, b, c)| {
+            move_stacks(a, b, c, stacks)
+        });
+
+    // Print each of the top of the stacks
+    for i in 0..stack_map.len() {
+        let mut value = stack_map.get(&(i as isize)).unwrap().to_owned();
+        let value_str = value.pop().unwrap();
+        print!("{}", value_str)
     }
-    // Create stacks
-    // Create a list of stacks, each containing the data
-
-    // Try to use a fold
-    // i.e iterate each item onto the stacks
-
-    println!("Blocks: {:?}", blocks);
-    println!("Moves: {:?}", moves);
 }
